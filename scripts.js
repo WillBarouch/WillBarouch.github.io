e93a14ebc8d4fe42e55a510b4cfbdc806860a57e
WillBarouch <93062709+WillBarouch@users.noreply.github.com>
2022-04-09 19:44:53 +1000

    Dark mode fixes

--- scripts.js
+++ scripts.js
@@ -3,15 +3,18 @@ let r = document.documentElement;
 
 r.style.setProperty('--body', 'var(--nord6)');
 r.style.setProperty('--cont', 'var(--nord0)');
+r.style.setProperty('--contless', 'var(--nord4)');
 
 function ModeToggle() {
     if(mode < 1) {
         r.style.setProperty('--body', 'var(--nord0)');
         r.style.setProperty('--cont', 'var(--nord6)');
+        r.style.setProperty('--contless', 'var(--nord3)');
         mode = 1;
     } else if (mode > 0){
         r.style.setProperty('--body', 'var(--nord6)');
         r.style.setProperty('--cont', 'var(--nord0)');
+        r.style.setProperty('--contless', 'var(--nord4)');
         mode = 0
     } else {
         window.alert("Dark mode broken, please reload");
