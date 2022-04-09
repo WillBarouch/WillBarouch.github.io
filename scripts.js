47d3d251a614ad1cfb6d8851781cb0bce390fa2e
WillBarouch <93062709+WillBarouch@users.noreply.github.com>
2022-04-09 12:56:03 +1000

    add mode var to localstorage for caching

--- scripts.js
+++ scripts.js
@@ -1,17 +1,20 @@
-let mode = 0;
+let mode = localStorage.getItem('mode') || 0;
 let r = document.documentElement;
 
 function ModeToggle() {
     if(mode === 0) {
-        mode = 1;
         r.style.setProperty('--body', 'var(--nord0)');
         r.style.setProperty('--cont', 'var(--nord6)');
-    } else if (mode === 1){
-        mode = 0;
+        mode = 1;
+        localStorage.setItem('mode', 1);
+    } else if (mode > 0){
         r.style.setProperty('--body', 'var(--nord6)');
         r.style.setProperty('--cont', 'var(--nord0)');
+        mode = 0;
+        localStorage.setItem('mode', 0);
     } else {
         window.alert("Dark mode broken, please reload");
     }
 }
+
 
