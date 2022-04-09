200b06355fa08df2c62e103f4fe23705b9b41830
WillBarouch <93062709+WillBarouch@users.noreply.github.com>
2022-04-09 18:59:56 +1000

    fix for dm error
    change mode toggle to </> from ===

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@ let mode = 0;
 let r = document.documentElement;
 
 function ModeToggle() {
-    if(mode === 0) {
+    if(mode < 1) {
         r.style.setProperty('--body', 'var(--nord0)');
         r.style.setProperty('--cont', 'var(--nord6)');
         mode = 1;
@@ -12,6 +12,7 @@ function ModeToggle() {
         mode = 0
     } else {
         window.alert("Dark mode broken, please reload");
+        console.log(mode)
     }
 }
 
