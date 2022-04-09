80a598e192d0e29b6a5076c58c2fb0b9221470be
WillBarouch <93062709+WillBarouch@users.noreply.github.com>
2022-04-09 12:48:19 +1000

    dark mode v2

--- scripts.js
+++ scripts.js
@@ -4,12 +4,12 @@ let r = document.documentElement;
 function ModeToggle() {
     if(mode === 0) {
         mode = 1;
-        r.style.setProperty('--body', 'var(--nord1)');
-        document.getElementById('dmtg').style.backgroundColor = 'var(--nord5)';
+        r.style.setProperty('--body', 'var(--nord0)');
+        r.style.setProperty('--cont', 'var(--nord6)');
     } else if (mode === 1){
         mode = 0;
         r.style.setProperty('--body', 'var(--nord6)');
-        document.getElementById('dmtg').style.backgroundColor = 'var(--nord1)';
+        r.style.setProperty('--cont', 'var(--nord0)');
     } else {
         window.alert("Dark mode broken, please reload");
     }
