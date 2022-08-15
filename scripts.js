dec5384f633b30a2970109c3365e78ce7cb5bb08
WillBarouch <93062709+WillBarouch@users.noreply.github.com>
2022-08-15 16:56:58 +1000

    fix waves on vertical devices, add toggle icon

--- scripts.js
+++ scripts.js
@@ -10,11 +10,15 @@ function ModeToggle() {
         r.style.setProperty('--body', 'var(--nord0)');
         r.style.setProperty('--cont', 'var(--nord6)');
         r.style.setProperty('--contless', 'var(--nord3)');
+        document.getElementById("Moon").style.visibility = 'hidden';
+        document.getElementById("Sun").style.visibility = 'visible';
         mode = 1;
     } else if (mode > 0){
         r.style.setProperty('--body', 'var(--nord6)');
         r.style.setProperty('--cont', 'var(--nord0)');
         r.style.setProperty('--contless', 'var(--nord4)');
+        document.getElementById("Moon").style.visibility = 'visible';
+        document.getElementById("Sun").style.visibility = 'hidden';
         mode = 0
     } else {
         window.alert("Dark mode broken, please reload");
