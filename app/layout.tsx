5aafda22c368bef2340aa94f9bab6a9325c56d5a
Will Barouch <willbarouch@gmail.com>
2024-04-21 16:40:22 +0100

    Add speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/react';
 import React from "react";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,6 +20,7 @@ export default function RootLayout({
     <html lang="en" >
       <body className={inter.className + "bg-background dark:bg-dark-background min-h-full"}>
           {children}
+          <SpeedInsights />
           <Analytics />
       </body>
     </html>
